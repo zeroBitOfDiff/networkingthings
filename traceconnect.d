106fcdb1e26d89bc74418e74186410369579e9df
zeroBit <luisivan001@knights.ucf.edu>
2021-04-19 13:17:39 -0400

    dtrace intro

---
+++ traceconnect.d
@@ -0,0 +1,14 @@
+struct sockaddr_in{
+  short             sin_family;
+  unsigned short    sin_port;
+  in_addr_t         sin_addr;
+  char              sin_zer[8];  
+};
+
+syscall::connect:entry
+/arg2 == sizeof(struct sockaddr_in)/
+{
+    addr = (struct sockaddr_in*)copyin(arg1, arg2);
+    printf("process:'%s' %s:%d", execname, inet_ntop(2, &addr->sin_addr),
+    ntohs(addr->sin_port));
+}
